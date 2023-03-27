2937cffcdc2dc1afa1c83e028fd2d7ad720b6e8d
lemschii <jonathan.lemmerer@gmail.com>
2023-03-27 08:30:55 +0200

    jz is einbundn
    kyskyskyskys

--- blutdruckrechner.js
+++ blutdruckrechner.js
@@ -2,21 +2,55 @@
 $(document).ready(function () {
     $("#btn_Calculate").click(function () {
       
-        var height = document.getElementById("heightInput").value;
-        var weight = document.getElementById("weightInput").value;
+        var sys = document.getElementById("sysInput").value;
+        var dia = document.getElementById("diaInput").value;
     
-        var result = (weight/(height/100*height/100)).toFixed(2);
+        //SYS AUSWERTUNG
 
-        if (result <= 18){
-            resulttext="Untergewicht";
+        if (sys <= 100){
+            resulttext1="Niedriger Blutdruck aufgrund des systolischen Wertes";
         }
-        else if (result >= 25){
-            resulttext="Übergewicht";
+        else if (sys >= 100 && sys<= 129){
+            resulttext1="Normaler systolischer Wert.";
         }
-        else {
-            resulttext="Normalgewicht";
+        else if (sys > 129 && sys <= 139 ){
+            resulttext1="Hochnormaler systolischer Wert";
+        }
+        else if (sys > 139 && sys<= 159){
+            resulttext1="Ihr systolischer Wert entspricht einer Hypertonie Grad 1.";
+        }
+        else if (sys > 159 && sys<= 179){
+            resulttext1="Ihr systolischer Wert entspricht einer Hypertonie Grad 2.";
+        }
+        else if (sys > 179){
+            resulttext1="Ihr systolischer Wert entspricht einer Hypertonie Grad 3.";
+        }
+        else if ( sys >= 140 && dia <= 90){
+            resulttext1="Isolierte systolische Hypertonie";
         }
 
-        document.getElementById("bmiOutput").textContent = result + ", also " + resulttext;
+
+        //DIA AUSWERTUNG
+
+        if (dia < 80){
+            resulttext2="Niedriger Blutdruck aufgrund des diastolischen Wertes";
+        }
+        else if (dia >= 80 && dia<= 84){
+            resulttext2="Normaler diastolischer Wert.";
+        }
+        else if (dia>84 && dia<=89){
+            resulttext2="Hochnormaler diastolischer Wert";
+        }
+        else if (dia>89 && dia<= 99){
+            resulttext2="Ihr diastolischer Wert entspricht einer Hypertonie Grad 1.";
+        }
+        else if (dia>99 && dia<=109){
+            resulttext2="Ihr diastolischer Wert entspricht einer Hypertonie Grad 2.";
+        }
+        else if (sys > 109){
+            resulttext2="Ihr diastolischer Wert entspricht einer Hypertonie Grad 3.";
+        }
+
+        document.getElementById("valuesOutput").textContent = resulttext1 + " " + resulttext2;
     });
 });
